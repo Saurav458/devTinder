e21b3ffc6f3b51d1ad47d0aca13efc84835e5d58
Saurav Jain <sauravjain@navneettoptech.com>
2024-10-12 23:19:05 +0530

    middlewares use and check authentication

--- src/app.js
+++ src/app.js
@@ -1,31 +1,23 @@
 const express = require("express");
 
 const app = express();
-// app.get("/test",(req,res,next)=>{
-//     // res.send("2ndresponse")
-//     next()
-// })
-// app.get("/test",(req,res,next)=>{
-//     console.log("1stuser")
-//     // next()
-// })
-const cb0 = function (req, res, next) {
-    console.log('CB0')
-    next()
-  }
-  
-  const cb1 = function (req, res, next) {
-    console.log('CB1')
-    next()
-  }
-  
-  const cb2 = function (req, res) {
-    res.send('Hello from C!')
-  }
-  
-  app.get('/test/c', [cb0, cb1, cb2])
-//   app.get('/test/c/:from-:to', [cb0, cb1, cb2])
 
+app.use("/admin", (req, res, next) => {
+  const token = "xyza";
+  if (token !== "xyz") {
+    res.status(401).send({ message: "user is not authorised" });
+  } else {
+    next();
+  }
+});
+
+app.get("/admin/getData", (req, res) => {
+  res.send("All data sent");
+});
+
+app.delete("/admin/getData", (req, res) => {
+  res.send("user is deleted");
+});
 
 app.listen(3000, () => {
   console.log(`server is listening at port${3000}`);
--- src/homework.md
+++ src/homework.md
@@ -2,3 +2,7 @@
 - orders or sequence of routes matter a lot
 
 - write logic to handle GET, POST,PUT,DELETE by adding routes and play with app.use make naming convention in postman
+
+- diff b/w app.use and app.all
+
+- middlewares are used to eliminate the redundancy of code ex- to check whether user is authorised or not
