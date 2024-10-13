a3b764e2ea8d68d7f7b81c1a4c024ad3c3f0fc6b
Saurav Jain <sauravjain@navneettoptech.com>
2024-10-13 15:00:14 +0530

    craete a dummy user using api call and save into db

--- src/app.js
+++ src/app.js
@@ -1,21 +1,37 @@
 const express = require("express");
 const connectDb = require("./config/database");
+const User=require('./models/user')
 const app = express();
 
+app.post("/signup", (req, res) => {
+  const userObj = {
+    firstName: "saurav",
+    lastName: "jain",
+    age: 20,
+    gender: "male",
+  };
+// create the instance of User Model instance like documents of User Class
+  const user=new User(userObj)
+   user.save().then((data)=>{
+    res.send({message:"user add successfully",data})
 
-const connectionDb=async()=>{
-    try{
-        const result=await connectDb()
-        if(result){
-            console.log("database connected successfully")
-            app.listen(7777,()=>{
-                console.log(`server is listening at port{7777}`)
-            })
-        }
-    }catch(err){
-        console.log(err.message)
+   }).catch((err)=>{
+    res.send({message:"something",error:err.message})
+
+   })
+});
+
+const connectionDb = async () => {
+  try {
+    const result = await connectDb();
+    if (result) {
+      console.log("database connected successfully");
+      app.listen(7777, () => {
+        console.log(`server is listening at port{7777}`);
+      });
     }
-   
-}
-connectionDb()
-
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+connectionDb();
